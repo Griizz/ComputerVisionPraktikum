f734ed3544c2177a8a93efbddff88974743b78c0
Griizz <Griizz-development@gmx.de>
2019-11-27 14:04:55 +0100

    Aufgabe 6.2 korregiert.

--- Termin 6/Aufgabe2.py
+++ Termin 6/Aufgabe2.py
@@ -5,15 +5,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
 from keras.optimizers import SGD
+from tensorflow import random
 
 import numpy as np
 
+# make results reproducible
 np.random.seed(123)
-
-from tensorflow import set_random_seed
-set_random_seed(123)
+random.set_seed(123)
 
 def change_labels(label):
+    '''
+    A function mapping values from teh set {1,4,8} to the set {0,1,2}
+    :param label: the current label from {1,4,8}
+    :return: returns the representing Label from {0,1,2}
+    '''
     if label == 1:
         return 0
 
@@ -47,57 +52,14 @@ for i in range(0, 60):
 for i in range(0, 30):
     vdLabels[i] = change_labels(vdLabels[i])
 
-#Hier drunter ist alt! Weiter mit 2.4
-
-trMatch = [0] * vdDesk.shape[0]
-deltaDesk = [0] * trDesk.shape[0]
-
-for i in range(vdDesk.shape[0]):
-    x = (trDesk - vdDesk[i])
-    for j in range(trDesk.shape[0]):
-        deltaDesk[j] = np.sqrt(np.sum((x[j, :]) ** 2))
-    n = deltaDesk.index(min(deltaDesk))
-    trMatch[i] = td["labels"][n]
-
-tp = sum(list(map(lambda x, y: x == y, trMatch, vd['labels'])))
-
-print("Trefferquote:", tp / len(vdDesk) * 100, "%")
-
-#1.4+
-new_trl = []
-new_val = []
-
-for l in trLabels:
-    if l == 1:
-        new_trl.append(0)
-    if l ==4:
-        new_trl.append(1)
-    if l ==8:
-        new_trl.append(2)
-        
-for v in vaLabels:
-    if v == 1:
-        new_val.append(0)
-    if v ==4:
-        new_val.append(1)
-    if v ==8:
-        new_val.append(2)
-    
-
-X_train =  trImages 
-X_test = vdImages
-
-
-y_train = np_utils.to_categorical(new_trl, 3)
-y_test = np_utils.to_categorical(new_val, 3)
+trLabels = np_utils.to_categorical(trLabels, 3)
+vdLabels = np_utils.to_categorical(vdLabels, 3)
 
 model = Sequential()
 
-model.add(Dense(60, activation='relu', name='fc1', 
-                  input_shape=(32,32,3)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu', name='fc2'))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(8, activation='relu', name='fc1', input_shape=(6,)))
+model.add(Dense(8, activation='relu', name='fc2'))
+model.add(Dense(3, activation='softmax', name='result'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer=SGD(lr=0.000005, momentum=0.9), 
@@ -105,10 +67,10 @@ model.compile(loss='categorical_crossentropy',
 
 
 
-model.fit(X_train, y_train, batch_size=60, nb_epoch=500, verbose=1)
+model.fit(trDesk, trLabels, batch_size=1, nb_epoch=500, verbose=1)
 
-score = model.evaluate(X_test, y_test, verbose=1)
+score = model.evaluate(vdDesk, vdLabels, verbose=1)
 
-print(score)
-#nach 500 Epochen acc= 50%
+print("Loss:", score[0], "\nAcc:", score[1])
+#nach 500 Epochen acc = 56.67%
 
