bdc51ef45d288e916f8fd6448e8e24ed311a519c
gottfriedoetzi <henry.apostle@gmx.de>
2020-01-08 16:52:18 +0100

    Maske + Mittelwert

--- Projekt/AnsatzHenry.py
+++ Projekt/AnsatzHenry.py
@@ -24,6 +24,47 @@ import glob
 import numpy as np
 from skimage.io import imread
 from os import walk
+from skimage.color import rgb2hsv
+from skimage.filters import threshold_otsu
+
+"""
+Gibt bei einer Liste von Bildern entsprechende Masken aus, die nach dem Otsu-Verfahren auf den Saturation-Dimensionen (HSV) maskiert.
+"""
+def createSMasks(imgs):
+    masks = []
+    for img in imgs:
+        imgS = rgb2hsv(img)[:, :, 1]
+        masks.append(imgS > threshold_otsu(imgS))
+    return masks
+
+def berechneMaskiertenMittelwert(img, mask):
+    summe = [0,0,0]
+    anzahlRelevanterPixel = 0
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            #if(not(img[x,y,0] == 0 and img[x,y,1] == 0 and img[x,y,2] == 0)):
+            if(mask[x,y] != 0):
+                anzahlRelevanterPixel +=1
+                summe += img[x,y]
+    return summe / anzahlRelevanterPixel
+
+def berechneMaskierteMittelwerte(imgs, masks):
+    mittelwerte = []
+    for i in range(len(imgs)):
+        mittelwerte.append(berechneMaskiertenMittelwert(imgs[i], masks[i]))
+    return mittelwerte
+
+
+testBild1 = imread("./DataSet/Carambola/Training/Carambola_135.png")
+imgS1 = rgb2hsv(testBild1)[:, :, 1]
+mask1 = (imgS1 > threshold_otsu(imgS1))
+testBild2 = imread("./DataSet/Carambola/Training/Carambola_136.png")
+imgS2 = rgb2hsv(testBild2)[:, :, 1]
+mask2 = (imgS2 > threshold_otsu(imgS2))
+testBild1HSV = rgb2hsv(testBild1)
+testBild2HSV = rgb2hsv(testBild2)
+mittelwerte = berechneMaskierteMittelwerte([testBild1HSV,testBild2HSV], [mask1,mask2])
+print(mittelwerte)
 
 #labels = ["Apple_Green", "Apple_Red", "Banana", "Carambola", "Guava", "Kiwi", "Mango", "Muskmelon", "Orange", "Peach", "Pear", "Persimmon", "Pitaya", "Plum", "Pomegranate", "Tomatoes"]
 _, labels, _ = walk("./DataSet").__next__()
@@ -57,3 +98,15 @@ for path in trStrings:
 testImgs = []
 for path in testStrings:
     testImgs.append(imread(path))
+
+print('Jetzt wird es interessant:')
+
+trMasks = createSMasks(trImgs)
+testMasks = createSMasks(testImgs)
+
+trMittelwerte = berechneMaskierteMittelwerte(trImgs, trMasks)
+testMittelwerte = berechneMaskierteMittelwerte(testImgs, testMasks)
+
+np.save('trMittelwerte',trMittelwerte)
+np.save('testMittelwerte', testMittelwerte)
+
--- Projekt/TestAusschneidenHenry.py
+++ Projekt/TestAusschneidenHenry.py
@@ -5,24 +5,40 @@ Bei manchen Obst funktioniert es besser als bei anderem: z.B Carambola sehr gut,
 from skimage.io import imread, imsave
 from skimage.color import rgb2hsv
 from skimage.filters import threshold_otsu
+from keras.utils import np_utils
+import cv2 as cv
 
-
-testBild = imread("./DataSet/Carambola/Training/Carambola_25.png")
+testBild = imread("./DataSet/Carambola/Training/Carambola_136.png")
 
 imsave("./TestAusschneidenHenry/TestBild.png", testBild)
 
 imgH = rgb2hsv(testBild)[:, :, 0]
 imgS = rgb2hsv(testBild)[:, :, 1]
 imgV = rgb2hsv(testBild)[:, :, 2]
-imgSV = imgS * imgV
+imgSV = 0.75* imgS + 0.25 * imgV
+
+#imgS_8bit = (imgS * 255).astype("uint8")
 
 imsave("./TestAusschneidenHenry/TestBildH.png", imgH)
 imsave("./TestAusschneidenHenry/TestBildS.png", imgS)
 imsave("./TestAusschneidenHenry/TestBildV.png", imgV)
 imsave("./TestAusschneidenHenry/TestBildSV.png", imgSV)
 
-mask = (imgSV > threshold_otsu(imgSV))
+#th_mean_grey = cv.adaptiveThreshold(imgS_8bit,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,11,2)
+
+
+mask = (imgS > threshold_otsu(imgS))
+
 
 ausgeschnitten  = testBild * mask[ :, :, None]
 
 imsave("./TestAusschneidenHenry/TestAusgeschnitten.png", ausgeschnitten)
+imsave("./TestAusschneidenHenry/TestMask.png", mask)
+
+#cv.imshow('Bild 2', mask)
+
+
+
+#cv.waitKey(0)
+#cv.destroyAllWindows()
+
