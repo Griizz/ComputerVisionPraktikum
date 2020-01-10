77cb7efd23a4363ec380e9ffcda5f94e1b17875f
gottfriedoetzi <henry.apostle@gmx.de>
2020-01-10 15:19:24 +0100

    Confusion Matrix

--- Projekt/Klassifiziere.py
+++ Projekt/Klassifiziere.py
@@ -1,6 +1,7 @@
 import numpy as np
+from os import walk
 
-def klassifiziere(trDesk, testDesk, trLabels):
+def klassifiziereNN(trDesk, testDesk, trLabels):
     predictions = []
     deltaDesk = [0] * len(trDesk)
 
@@ -12,14 +13,42 @@ def klassifiziere(trDesk, testDesk, trLabels):
         predictions.append(trLabels[n])
     return predictions
 
+def ConfusionMatrix(prediction, validation, labelCount):
+    '''
+
+    :param prediction: the 1-Dimensional Array of predicted Labels
+    :param validation: the 1-Dimensional Array of actual Labels
+    :param labelCount: the numer of labels
+    :return: a labelCount x labelCount ConfusionMatrix
+    '''
+
+    matrix = np.zeros((labelCount, labelCount), dtype=int)
+
+    for i in range(len(prediction)):
+        matrix[validation[i], prediction[i]] += 1
+
+    return matrix
+
+_, labels, _ = walk("./DataSet").__next__()
+
+#Wähle Mittelwert als Deskriptor
 trDesk = np.load('trMittelwerte.npy')
 testDesk = np.load('testMittelwerte.npy')
 
 trLabels = np.load('trLabels.npy')
 testLabels = np.load('testLabels.npy')
 
-predictions = klassifiziere(trDesk, testDesk, trLabels)
+predictions = klassifiziereNN(trDesk, testDesk, trLabels)
 
+
+#Berechnung der Trefferquote
 evaluatedPredictions = predictions == testLabels
+#for i in range(len(predictions)):
+#    if predictions[i] != testLabels[i]:
+#        print(labels[testLabels[i]] + " seen as " + labels[predictions[i]])
 correctPredictions = sum(evaluatedPredictions)
 print("Trefferquote:", correctPredictions / len(testLabels) * 100, "%")
+
+#Berechnung der Confusion Matrix
+cMatrix = ConfusionMatrix(predictions, testLabels, 16)
+print(cMatrix)
