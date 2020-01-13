97bce0f9f8237d757907ef6a7ccfcee91396004a
Griizz <Griizz-development@gmx.de>
2020-01-13 09:51:31 +0100

    Fixed Model

--- Projekt/CNN-Ansatz/TEST.py
+++ Projekt/CNN-Ansatz/TEST.py
@@ -2,56 +2,57 @@ import os
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 from keras.optimizers import SGD
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import wandb
 from wandb.keras import WandbCallback
 
 wandb.init(project="cv_project")
 
-train_datagen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 val_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     '../DataSetNew/Training',
     target_size=(256, 256),
-    batch_size=32,)
+    color_mode="rgb",
+    class_mode="categorical",
+    batch_size=16)
 
 validation_generator = val_datagen.flow_from_directory(
     '../DataSetNew/Validation',
     target_size=(256, 256),
-    batch_size=32,)
+    color_mode="rgb",
+    class_mode="categorical",
+    batch_size=16)
 
 model = Sequential()
 
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1', input_shape=(256, 256, 3)))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', name='conv2'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (8, 8), activation='relu', padding='same', name='conv3'))
-model.add(Conv2D(32, (8, 8), activation='relu', padding='same', name='conv4'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='conv2'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (8, 8), activation='relu', padding='same', name='conv5'))
-model.add(Conv2D(32, (8, 8), activation='relu', padding='same', name='conv6'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='conv3'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(1024, activation='relu', name='fc1', ))
+model.add(Dense(256, activation='relu', name='fc1', ))
 model.add(Dense(16, activation='softmax'))  # Für jedes Label ein output
 
 modelCheckpoint = ModelCheckpoint("./Best.h5", monitor='val_loss', verbose=0, save_best_only=True,
                                   save_weights_only=False, mode='auto', period=1)
 
-reduceLROnPlateau = ReduceLROnPlateau(monitor='val_loss', factor=0.0,
-                              patience=5, min_lr=0.001)
+reduceLROnPlateau = ReduceLROnPlateau(monitor='val_loss', factor=0.25,
+                              patience=5, min_lr=0.0005)
 
 model.compile(loss='categorical_crossentropy',
               optimizer=SGD(lr=0.01, momentum=0.9),
               metrics=['accuracy'])
 
 model.fit_generator(train_generator,
-                    steps_per_epoch=800,  # 16 * 800 / 32 (Num_cat * pics_cat / batchSize)
+                    steps_per_epoch=800,  # 16 * 800 / 16 (Num_cat * pics_cat / batchSize)
                     epochs=50,
                     validation_data=validation_generator,
-                    validation_steps=75,  # 16 * 150 / 32 (Num_cat * pics_cat / batchSize)
+                    validation_steps=150,  # 16 * 150 / 16 (Num_cat * pics_cat / batchSize)
                     callbacks=[modelCheckpoint, WandbCallback(), reduceLROnPlateau])
 
 
